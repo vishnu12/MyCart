cdb9fd81ceb36668e0d7137d9ea91b10772d0b8c
Vishnu <vishnu007jr@gmail.com>
2020-11-13 14:56:55 +0530

    prod-chaanges

--- server/server.js
+++ server/server.js
@@ -29,6 +29,18 @@ app.get('/config/paypal',(req,res)=>{
 const __dirname=path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'/client/build')))
+    app.use('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+ }else{
+     app.get('/',(req,res)=>{
+         res.send('API is running')
+     })
+ }
+
+
 app.use(notFound)
 app.use(errorHandler)
 
